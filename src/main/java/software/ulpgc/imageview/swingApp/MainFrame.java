2e1aa5ac888c4b517829fa1e726e6efb1fcf7126
TheTatsujin <luis.martin119@alu.ulpgc.es>
2024-12-18 22:30:07 +0000

    Add automatic scaling

--- src/main/java/software/ulpgc/imageview/swingApp/MainFrame.java
+++ src/main/java/software/ulpgc/imageview/swingApp/MainFrame.java
@@ -16,7 +16,7 @@ public class MainFrame extends JFrame {
     public MainFrame() {
         this.commands = new HashMap<>();
         setTitle("Image Viewer");
-        setSize(Toolkit.getDefaultToolkit().getScreenSize());
+        setSize(800, 600);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
--- src/main/java/software/ulpgc/imageview/swingApp/SwingImageDisplay.java
+++ src/main/java/software/ulpgc/imageview/swingApp/SwingImageDisplay.java
@@ -24,11 +24,37 @@ public class SwingImageDisplay extends JPanel implements ImageDisplay {
     public void paint(Graphics g) {
         g.setColor(Color.BLACK);
         g.fillRect(0, 0, getWidth(), getHeight());
-        g.drawImage(image.content(), 50, 50, getWidth()-100, getHeight()-50, null);
+        Dimension scaledImage = fitToResolution(image);
+        g.drawImage(
+                image.content(),
+                offset(scaledImage).width,
+                offset(scaledImage).height,
+                scaledImage.width,
+                scaledImage.height,
+                null
+        );
     }
 
-    private int fitToResolution(Image image) {
-        return 0;
+    private Dimension fitToResolution(Image image) {
+        Dimension imageSize = new Dimension(image.content().getWidth(), image.content().getHeight());
+        return imageSize.scale(Math.min(
+                (double) this.getWidth() / imageSize.width(),
+                (double) this.getHeight() / imageSize.height()
+        ));
+    }
+
+    private Dimension offset(Dimension imageSize) {
+        return new Dimension(
+                (getWidth() - imageSize.width) / 2,
+                (getHeight() - imageSize.height) / 2
+        );
+    }
+
+
+    private record Dimension(int width, int height) {
+        public Dimension scale(double factor) {
+            return new Dimension((int) (width * factor), (int) (height * factor));
+        }
     }
 
 }
